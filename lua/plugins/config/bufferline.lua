f1ece875e7b656da31037cce99d5f04350127314
ismailshak <ismailshak94@yahoo.com>
2022-06-10 09:43:40 -0400

    Update some mappings

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -7,14 +7,14 @@ end
 
 -- keybindings
 local utils = require("utils.keybindings")
-utils.keymap("n", "<leader>bn", ":BufferLineCycleNext <CR>")
-utils.keymap("n", "<leader>bv", ":BufferLineCyclePrev <CR>")
+utils.keymap("n", "<TAB>", ":BufferLineCycleNext <CR>")
+utils.keymap("n", "<S-TAB>", ":BufferLineCyclePrev <CR>")
 
 bufferline.setup({
 	options = {
 		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
 		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
 		-- NOTE: this plugin is designed with this icon in mind,
--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -5,7 +5,7 @@ end
 
 -- keybindings
 local utils = require("utils.keybindings")
-utils.keymap("n", "<c-n>", ":NvimTreeToggle <CR>")
+utils.keymap("n", "<c-n>", ":NvimTreeFindFileToggle <CR>")
 
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 -- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`).
