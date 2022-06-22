d5be802eb5a0cb1ddbdac553bd4132791aab79a6
ismailshak <ismailshak94@yahoo.com>
2022-06-22 10:35:52 -0400

    Switch to goimports + autoformat on write

--- README.md
+++ README.md
@@ -6,8 +6,15 @@
 brew install stylua codespell
 ```
 
+```bash
+go install golang.org/x/tools/cmd/goimports@latest
+```
+
+Make sure you have go's bin dir on your PATH: `export PATH="$PATH:$HOME/go/bin"`
+
 ## LSPs installed
 
 ```vim
 :LspInstall cssls gopls jsonls tsserver html dockerls sumneko_lua marksman yamlls
 ```
+
--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -3,22 +3,37 @@ if not null_ls_status_ok then
 	return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
-local completion = null_ls.builtins.completion
+-- local completion = null_ls.builtins.completion
 
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier,
 		formatting.stylua,
-		formatting.gofmt,
-		-- formatting.goimports, -- TODO: set this up, will replace gofmt and will handle auto imports
+		formatting.goimports, -- fixes imports and formats the same way `gofmt` does
 		diagnostics.eslint,
 		diagnostics.codespell,
 		code_actions.gitsigns,
 		code_actions.eslint,
 	},
+  -- Format on write
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
