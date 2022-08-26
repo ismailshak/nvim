d1f8faa2de66511601dddbb3dc982c499cc0afe0
ismailshak <ismailshak94@yahoo.com>
2022-08-26 12:06:20 +0200

    Comments ignore empty lines

--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -9,7 +9,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
--- local completion = null_ls.builtins.completion
+--[[ local completion = null_ls.builtins.completion ]]
 
 null_ls.setup({
 	debug = false,
@@ -22,7 +22,7 @@ null_ls.setup({
 		code_actions.gitsigns,
 		code_actions.eslint,
 	},
-  -- Format on write
+	-- Format on write
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
