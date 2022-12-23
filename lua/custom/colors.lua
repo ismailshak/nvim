62b7d42884eb12f89728ad714ce3cfe48eeaaa07
ismailshak <ismailshak94@yahoo.com>
2022-12-23 09:04:22 -0500

    Lighter virtual text & better git colors

--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -8,17 +8,18 @@ vim.g.nord_bold = 0
 vim.g.tokyonight_style = "night" -- default "storm"
 
 -- iceberg
--- override some highlight groups
--- adding:
--- gitsigns support
--- fidget support
--- fix some colors (virtual text, functions etc)
--- fix git diff colors
--- fix border
+--[[
+  override some highlight groups
+  adding:
+  - fix git diff colors
+  - fidget support
+  - fix some colors (virtual text, functions etc)
+  - fix border
+]]--
 vim.cmd([[
   augroup IcebergColors
     autocmd!
-    autocmd ColorScheme iceberg hi NonText ctermfg=236 guifg=#323a59
+    autocmd ColorScheme iceberg hi NonText guifg=#3f4660
       \ | hi Type ctermfg=110 gui=NONE guifg=#a093c7
       \ | hi Keyword ctermfg=110 gui=NONE guifg=#84a0c6
       \ | hi TSFunction ctermfg=252 guifg=#b4be82
@@ -26,10 +27,6 @@ vim.cmd([[
       \ | hi DiffChange guibg=#32382e guifg=NONE
       \ | hi DiffDelete guibg=#53343b guifg=NONE
       \ | hi DiffText guibg=#4c5340 guifg=NONE
-      \ | hi GitSignsChange ctermbg=235 guibg=#1e2132
-      \ | hi GitSignsChangeLn ctermbg=235 ctermfg=109 guibg=#1e2132 guifg=#2d539e
-      \ | hi GitSignsChangeLn ctermbg=bg ctermfg=109 guibg=#1e2132 guifg=#2d539e
-      \ | hi GitSignsChangeDelete ctermbg=235 ctermfg=109 guibg=#1e2132 guifg=#2d539e
       \ | hi! link TSConstructor TSFunction
       \ | hi! link TSProperty TSKeyword
       \ | hi! link NormalFloat Normal
