61a9b9446adcf50717c6483801ad423cc68a3f3f
ismailshak <ismailshak94@yahoo.com>
2023-03-01 07:54:36 -0500

    Fix tokyonight theme

--- lua/custom/colors/tokyonight.lua
+++ lua/custom/colors/tokyonight.lua
@@ -1,11 +1,17 @@
 local helpers = require("utils.helpers")
-if not helpers.exists("") then
+if not helpers.exists("tokyonight") then
 	return
 end
 
 require("tokyonight").setup({
-	style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 	light_style = "day", -- The theme is used when the background is set to light
 	sidebars = { "qf", "help", "terminal", "packer", "NvimTree" }, -- Set a darker background on sidebar-like windows.
 	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+	styles = {
+		-- Style to be applied to different syntax groups
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = false },
+		keywords = { italic = false },
+	},
 })
