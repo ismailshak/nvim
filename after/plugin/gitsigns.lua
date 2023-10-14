978b08e0bb5980e51e0145ce8e4cc703e8dc9ef7
ismailshak <ismailshak94@yahoo.com>
2023-10-14 18:28:45 -0400

    fix: change gitsigns untracked sign

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -7,11 +7,12 @@ end
 
 require("gitsigns").setup({
 	signs = {
-		add = { hl = "GitSignsAdd", text = "|", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "|", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add = { text = "|" },
+		change = { text = "|" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┃" },
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -30,14 +31,14 @@ require("gitsigns").setup({
 		ignore_whitespace = false,
 	},
 	--current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-	current_line_blame_formatter = "        <author>, <author_time:%R> - <summary>",
+	current_line_blame_formatter = "   <author>, <author_time:%R> - <summary>",
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
 	max_file_length = 40000,
 	preview_config = {
 		-- Options passed to nvim_open_win
-		border = "single",
+		border = "rounded",
 		style = "minimal",
 		relative = "cursor",
 		row = 0,
