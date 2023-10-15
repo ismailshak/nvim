208679705215f9ce5cce62e9371472a9c505930d
ismailshak <ismailshak94@yahoo.com>
2023-10-15 09:31:20 -0400

    feat: extend code action mapping to visual mode

--- lua/lsp_settings/on-attach.lua
+++ lua/lsp_settings/on-attach.lua
@@ -1,35 +1,40 @@
+local api = require("utils.api")
+
 local M = {}
 
+M.gen_desc = function(desc)
+	return desc .. " (LSP)"
+end
+
 function M.on_attach(client, bufnr)
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = desc .. " [LSP]"
-		end
+	local opts = { buffer = bufnr }
+	api.nmap("<leader>rn", vim.lsp.buf.rename, M.gen_desc("[R]e[n]ame"), opts)
+	api.nmap("<leader>ca", vim.lsp.buf.code_action, M.gen_desc("[C]ode [A]ction"), opts)
+	api.vmap("<leader>ca", vim.lsp.buf.code_action, M.gen_desc("Selected range [C]ode [A]ction"), opts)
 
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
-
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	api.nmap("gd", vim.lsp.buf.definition, M.gen_desc("[G]oto [D]efinition"), opts)
+	api.nmap("gr", require("telescope.builtin").lsp_references, M.gen_desc("[G]oto [R]eferences"), opts)
+	api.nmap("gI", vim.lsp.buf.implementation, M.gen_desc("[G]oto [I]mplementation"), opts)
+	api.nmap("<leader>D", vim.lsp.buf.type_definition, M.gen_desc("Type [D]efinition"), opts)
+	api.nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, M.gen_desc("[D]ocument [S]ymbols"), opts)
+	api.nmap(
+		"<leader>ws",
+		require("telescope.builtin").lsp_dynamic_workspace_symbols,
+		M.gen_desc("[W]orkspace [S]ymbols"),
+		opts
+	)
 
 	-- See `:help K` for why this keymap
-	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-i>", vim.lsp.buf.signature_help, "Signature Documentation")
+	api.nmap("K", vim.lsp.buf.hover, M.gen_desc("Hover Documentation"), opts)
+	api.nmap("<C-i>", vim.lsp.buf.signature_help, M.gen_desc("Signature Documentation"), opts)
 
 	-- Lesser used LSP functionality
-	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	nmap("<leader>wl", function()
+	api.nmap("gD", vim.lsp.buf.declaration, M.gen_desc("[G]oto [D]eclaration"), opts)
+	api.nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, M.gen_desc("[W]orkspace [A]dd Folder"), opts)
+	api.nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, M.gen_desc("[W]orkspace [R]emove Folder"), opts)
+	api.nmap("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
+	end, M.gen_desc("[W]orkspace [L]ist Folders"), opts)
 
 	-- Disable LSP formatting
 	client.server_capabilities.document_formatting = false
