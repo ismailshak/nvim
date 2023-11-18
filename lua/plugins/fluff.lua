cd6baf4e45dec9201a4d29f8ad895391e8ea4c27
ismailshak <ismailshak94@yahoo.com>
2023-11-18 18:40:45 -0500

    feat: use noice for hover docs

--- lua/plugins/fluff.lua
+++ lua/plugins/fluff.lua
@@ -133,4 +133,64 @@ return {
 			})
 		end,
 	},
+
+	-- Prettier UI for certain things
+	-- (syntax highlighting in hover docs)
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			cmdline = { enabled = false },
+			messages = { enabled = false },
+			popupmenu = { enabled = false },
+			notify = { enabled = false },
+			lsp = {
+				progress = { enabled = false },
+				override = {
+					-- override the default lsp markdown formatter with Noice
+					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+					-- override the lsp markdown formatter with Noice
+					["vim.lsp.util.stylize_markdown"] = false,
+					-- override cmp documentation with Noice (needs the other options to work)
+					["cmp.entry.get_documentation"] = false,
+				},
+				hover = {
+					enabled = true,
+					silent = false, -- set to true to not show a message if hover is not available
+				},
+				signature = {
+					enabled = true,
+					auto_open = {
+						enabled = true,
+						trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+						luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+						throttle = 50, -- Debounce lsp signature help request by 50ms
+					},
+					view = nil, -- when nil, use defaults from documentation
+					---@type NoiceViewOptions
+					opts = {}, -- merged with defaults from documentation
+				},
+				message = { enabled = false },
+				-- defaults for hover and signature help
+				documentation = {
+					view = "hover",
+					---@type NoiceViewOptions
+					opts = {
+						lang = "markdown",
+						replace = true,
+						render = "plain",
+						format = { "{message}" },
+						border = "single",
+					},
+				},
+			},
+			presets = {
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+		},
+	},
 }
