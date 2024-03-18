747dfdfe58f68a5afd582360e41026267be5080d
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-03-18 09:29:51 -0400

    fix: use opt_local for ftplugin files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 -- Wrap lines when they go off screen
-vim.opt.wrap = true
+vim.opt_local.wrap = true
 
 -- Wrap should break at words, not letters
-vim.opt.linebreak = true
+vim.opt_local.linebreak = true
