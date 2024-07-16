79244521d0104f5e9ee01f7dec5e5b2aa5a32b3f
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-07-16 14:14:41 -0400

    fix: better settings type annotations

--- lua/custom/settings/defaults.lua
+++ lua/custom/settings/defaults.lua
@@ -1,24 +1,30 @@
+---@alias background 'dark' | 'light'
+
+---@alias theme
+---| 'iceberg'
+---| 'oxocarbon'
+---| 'nord'
+---| 'rose-pine'
+---| 'carbonfox'
+---| 'nightfox'
+---| 'dayfox'
+---| 'catppuccin'
+---| 'substrata'
+
+---@class DBConnection
+---@field public name string: Connection name
+---@field public url string: Connection URL
+---
 ---@class Settings
+---@field public background background
+---@field public theme theme
+---@field public disable_format string: Comma-separated list of directories where formatting should be disabled
+---@field public db_connections DBConnection[]: List of database connections
+
+---@type Settings
 local M = {
-	---@alias background 'dark' | 'light'
-	---@type background
 	background = "dark",
-
-	---@alias theme
-	---| 'iceberg'
-	---| 'oxocarbon'
-	---| 'nord'
-	---| 'rose-pine'
-	---| 'carbonfox'
-	---| 'nightfox'
-	---| 'dayfox'
-	---| 'catppuccin'
-	---| 'substrata'
-	--@type theme
 	theme = "iceberg",
-
-	---Comma-separated list of directories where formatting should be disabled
-	---@type string
 	disable_format = "",
 }
 
