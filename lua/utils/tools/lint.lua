9b3f3904a8cfb71ef1f52ccd117a5675cdb25689
Ismail Elshakankiry <ismailshak94@yahoo.com>
2024-10-29 13:40:20 -0400

    fix: wrong log level

--- lua/utils/tools/lint.lua
+++ lua/utils/tools/lint.lua
@@ -61,7 +61,7 @@ function M.lint()
 	names = vim.tbl_filter(function(name)
 		local linter = lint.linters[name]
 		if not linter then
-			vim.notify("Linter not found: " .. name, vim.notify.levels.WARN)
+			vim.notify_once("Linter not found: " .. name, vim.log.levels.WARN)
 		end
 
 		return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
